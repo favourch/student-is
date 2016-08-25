0f914ffaadbebc54970b82f7205507ee0daa325a
Geoffrey Barnard <geoffreybans@gmail.com>
2016-08-25 12:17:42 +0300

    Creating the main.js file for loading requirejs library

---
+++ public/js/main.js
@@ -0,0 +1,72 @@
+/*global require*/
+'use strict';
+
+// Require.js allows us to configure shortcut alias
+require.config({
+	// The shim config allows us to configure dependencies for
+	// scripts that do not call define() to register a module
+	shim: {
+		underscore: {
+			exports: '_'
+		},
+		backbone: {
+			deps: [
+				'underscore',
+				'jquery'
+			],
+			exports: 'Backbone'
+		},
+		backboneLocalstorage: {
+			deps: ['backbone'],
+			exports: 'Store'
+		},
+		bootstrap:{
+			deps: ['jquery']
+		}
+	},
+	paths: {
+		jquery: 'libs/jquery',
+		underscore: 'libs/underscore',
+		backbone: 'libs/backbone',
+		bootstrap: 'libs/bootstrap.min',
+		backboneLocalstorage: 'libs/backbone.localstorage',
+		text: 'libs/require.text'
+	}
+});
+
+require([
+	'backbone',
+	'routers/router', 
+	'views/index',
+	'views/home', 
+	'views/blog', 
+	'views/about', 
+	'views/contact', 
+	], function (
+		Backbone, Router, IndexView, HomeView,
+		BlogView, AboutView, ContactView) {
+	
+	// Initialize routing and start Backbone.history()
+	var App = new Router();
+
+	App.on('route:home', function(){
+		new HomeView;
+	});
+
+	App.on('route:blog', function(){
+		new BlogView;
+	});
+
+	App.on('route:about', function(){
+		new AboutView;
+	});
+
+	App.on('route:contact', function(){
+		new ContactView;
+	});
+
+	Backbone.history.start();
+
+	// Initialize the application view
+	new IndexView;
+});
