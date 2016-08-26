b7b5a60c8b7e07ae9cc0a20fc543974b390604e3
Geoffrey Barnard <geoffreybans@gmail.com>
2016-08-26 18:00:58 +0300

    Creating the Admin controller class

---
+++ application/controllers/AdminController.php
@@ -0,0 +1,43 @@
+<?php namespace Controllers;
+
+/**
+ *This class loads the application homepage
+ *@author Geoffrey Oliver <geoffreybans@gmail.com>
+ *@copyright 2015 - 2020 Geoffrey Bans
+ *@category Controllers
+ *@package Controllers\Admin
+ *@link https://github.com/geoffreybans/student-is
+ *@license http://opensource.org/licenses/MIT MIT License
+ *@version 1.0.1
+ */
+
+use Drivers\Templates\View;
+use Libraries\CronLibrary\SampleCronController;
+use Models\UsersModel;
+use Helpers\Url\Url;
+use Helpers\Input\Input;
+
+class AdminController extends BaseController {
+
+	/**
+	 * @var bool Set to true to enable method filters in this controller
+	 */
+	public $enable_method_filters = false;
+
+	/**
+	 * This method loads the homepage. 
+	 * @param int $id The user id
+	 * @return void
+	 */
+	public function getIndex( $id)
+	{
+
+		$data['title'] = $this->site_title;
+		$data['request_time'] = $this->request_exec_time();
+
+		View::render('home.index',$data);
+
+	}
+	
+}
+
