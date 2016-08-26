0bdc9f569e50538d029709121e276d15c9dad00d
Geoffrey Barnard <geoffreybans@gmail.com>
2016-08-26 18:03:22 +0300

    Loading the css for the landing page using backbonejs

--- public/js/views/home.js
+++ public/js/views/home.js
@@ -15,6 +15,12 @@ define([
 		template: _.template(IndexTemplate),
 
 		initialize: function(){
+			//this will ensure the css is only added once
+			if ($("#customCSS").length == 0) {
+				//this will add the current css to the page 
+				$('head').append('<link rel="stylesheet" id="customCSS" href="css/business-casual.css">');
+			}         
+
 			$("title").html(this.title);
 			this.$el.html(this.template());
 		}
