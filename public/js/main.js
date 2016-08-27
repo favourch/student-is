af334ea920aa4acd2bd4645b63351212ad6c79c3
Geoffrey Barnard <geoffreybans@gmail.com>
2016-08-27 13:54:15 +0300

    Defining the admissions and academics routes event listeners in the main.js file

--- public/js/main.js
+++ public/js/main.js
@@ -51,43 +51,53 @@ require([
 	'views/about', 
 	'views/contact',
 	'views/admin',
-	'views/dashboard'	], function (
-		Backbone, Router, LoginView, HomeView,
-		SignupView, AboutView, ContactView, AdminView, DashView) {
+	'views/dashboard',
+	'views/admissions',
+	'views/academics'	], function (
+		Backbone, Router, Login, Home, Signup, About, Contact, 
+		Admin, Dashboard, Admissions, Academics) {
 	
 	// Initialize routing and start Backbone.history()
 	var App = new Router();
 
 	App.on('route:login', function(){
-		new LoginView;
+		new Login;
 	});
 
 	App.on('route:home', function(){
-		new HomeView;
+		new Home;
 	});
 
 	App.on('route:signup', function(){
-		new SignupView;
+		new Signup;
 	});
 
 	App.on('route:about', function(){
-		new AboutView;
+		new About;
 	});
 
 	App.on('route:contact', function(){
-		new ContactView;
+		new Contact;
 	});
 
 	App.on('route:admin', function(){
-		new AdminView;
+		new Admin;
 	});	
 
 	App.on('route:dashboard', function(){
-		new DashView;
+		new Dashboard;
+	});	
+
+	App.on('route:admissions', function(){
+		new Admissions;
+	});	
+
+	App.on('route:academics', function(){
+		new Academics;
 	});
 
 	Backbone.history.start();
 
 	// Initialize the application view
-	new HomeView;
+	new Home;
 });
