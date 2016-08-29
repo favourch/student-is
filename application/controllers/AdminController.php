ee7713dc6bcd8b0c79e1fd1756a1194c4254bbdc
Geoffrey Barnard <geoffreybans@gmail.com>
2016-08-30 02:06:57 +0300

    Sending email after account creation for new users

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -38,12 +38,12 @@ class AdminController extends BaseController {
 		//invalid token used/no access token provided
 		if (!$tokenExists->num_rows()) {		 
 			header('HTTP/1.0 403 Forbidden');
-			die('Login required!');
+			die('Login required!1');
 		}
 		//check for expired token
 		elseif ($tokenExists->result_array()[0]['logout'] === true) {
 			header('HTTP/1.0 403 Forbidden');
-			die('Login required!');
+			die('Login required!2');
 		}
 		//unathorized user access to admin controller
 		elseif ($tokenExists->result_array()[0]['user_role'] != 1 AND $tokenExists->result_array()[0]['user_role'] != 2) {
@@ -57,7 +57,7 @@ class AdminController extends BaseController {
 
 			if (($timestamp + $token['duration']) > time()) {
 				header('HTTP/1.0 403 Forbidden');
-				die('Login required!');
+				die('Login required!3');
 			}
 
 			//extend token lifespan
@@ -73,10 +73,10 @@ class AdminController extends BaseController {
 	/**
 	 * This method returns a list of all clients in the database
 	 * @before auth
-	 * @param string $token The access token string
+	 * @param null
 	 * @return JSON object of clients
 	 */
-	public function getClients($token){
+	public function getClients(){
 
 		$clients = ClientModel::all();
 		View::renderJSON($clients->result_array());
@@ -85,10 +85,10 @@ class AdminController extends BaseController {
 	/**
 	 * This method adds a new client to the database
 	 * @before auth
-	 * @param string $token The token to authenticate request
+	 * @param null
 	 * @return JSON
 	 */
-	public function postClients($token){
+	public function postClients(){
 
 		$client = json_decode($_POST['model']);
 		
@@ -104,8 +104,29 @@ class AdminController extends BaseController {
 		);
 
 		$create = ClientModel::save($client);
+		$clientId = $create->lastInsertId;
+
+		$password = substr(md5(uniqid(mt_rand(), true)), 0, 8);
+		UserModel::save(array(
+			'first_name' => $client->first_name,
+			'last_name' => $client->last_name,
+			'email' => $client->email,
+			'password' => md5(sha1($client->email)),
+			'client_id' => $clientId
+			)
+		);
+
+		// The message
+		$message = "Hello {$client->first_name} {$client->last_name} your account for Student Infomation System software has been created succeddfully.\r\n";
+		$message += "Email: {$client->email} \r\n Password: $password\r\n Institution: $client->institution\r\n  You can login using this link Url::base()";
+		$headers = "From: geoffreybans@gmail.com \r\n Reply-To: geoffreybans@gmail.com \r\n X-Mailer: PHP/ phpversion()";
+		// In case any of our lines are larger than 70 characters, we should use wordwrap()
+		$message = wordwrap($message, 70, "\r\n");
+
+		// Send email
+		mail($client->email, 'Student IS - Account Created', $message, $headers);
+
 		$new = ClientModel::getById($create->lastInsertId());
-		
 		View::renderJSON($new->result_array()[0]);
 
 	}
@@ -113,10 +134,10 @@ class AdminController extends BaseController {
 	/**
 	 * This  methods returns all the users in the database
 	 * @before auth
-	 * @param string $token The access token
+	 * @param null
 	 * @return JSON
 	 */
-	public function getUsers($token){
+	public function getUsers(){
 
 		$users = UserModel::all();
 		View::renderJSON($users->result_array());
@@ -125,24 +146,33 @@ class AdminController extends BaseController {
 	/**
 	 * This method adds a new user to the database
 	 * @before auth
-	 * @param string $token The token to authenticate request
+	 * @param null
 	 * @return JSON
 	 */
-	public function postUsers($token){
+	public function postUsers(){
 		
 		$user = json_decode($_POST['model']);
 		
 		$client = array(
 			'first_name' => $user->first_name,
 			'last_name' => $user->last_name,
-			'password' => md5($user->password),
+			'password' => md5(sha1($user->password)),
 			'email' => $user->email,			
 			'user_role' => $user->user_type
 		);
 
 		$create = UserModel::save($client);
-		$new = UserModel::getById($create->lastInsertId());
+
+		// The message
+		$message = "Hello {$user->first_name} {$user->last_name} your account for Student Infomation System software has been created.\r\n";
+		$message += "Email: {$user->email} \r\n Password: $user->password\r\n  You can login using this link Url::base()";
+		$headers = "From: geoffreybans@gmail.com \r\n Reply-To: geoffreybans@gmail.com \r\n X-Mailer: PHP/ phpversion()";
+		// In case any of our lines are larger than 70 characters, we should use wordwrap()
+		//$message = wordwrap($message, 70, "\r\n");
+		// Send email
+		mail($user->email, 'Student IS - Account Created', $message, $headers);
 		
+		$new = UserModel::getById($create->lastInsertId());
 		View::renderJSON($new->result_array()[0]);	
 	}
 
