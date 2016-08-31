654f039b3c8da1de6aded012363235f59f00e0d4
Geoffrey Barnard <geoffreybans@gmail.com>
2016-08-31 22:37:32 +0300

    Creating the model for new students

---
+++ public/js/models/student.js
@@ -0,0 +1,32 @@
+define([
+	'underscore',
+	'backbone'
+	], function(_, Backbone){
+
+	var Student = Backbone.Model.extend({
+
+		defaults: {
+			first_name: '',
+			middle_name: '',
+			last_name: '',
+			dob: '',
+			pob: '',
+			bcn: '',
+			sex: '',
+			nationality: '',
+			doa: '',
+			class: '',
+			address: '',
+			code: '',
+			town: '',
+			pg_f_name: '',
+			pg_l_name: '',
+			pg_email: '',
+			pg_phone: ''
+		}
+
+	});
+
+	return Student;
+
+});
