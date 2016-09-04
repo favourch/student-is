afa860696c3bdf387097bb920dfe58cbeda8c460
Geoffrey Barnard <geoffreybans@gmail.com>
2016-09-05 01:15:00 +0300

    Creating the models for handling subjects, streams and exams in the database

---
+++ application/models/ExamModel.php
@@ -0,0 +1,26 @@
+<?php namespace Models;
+
+/**
+ * This model creates exams in the database for a client
+ * @author Geoffrey Bans <geoffreybans@gmail.com>
+ * @copyright 2015 - 2020 Geoffrey Bans
+ * @category Models
+ * @package Models\ExamModel
+ * @link https://github.com/geoffreybans/student-is
+ * @license http://opensource.org/licenses/MIT MIT License
+ * @version 1.0.1
+ */
+
+class ExamModel extends Model {
+
+	/**
+	 * @var string The name of the table associated with this model
+	 */
+	protected static $table = 'exams';
+
+	/**
+	 * @var bool Set whether query timestamps should be updated
+	 */	
+	protected static $update_timestamps = true;
+
+}
