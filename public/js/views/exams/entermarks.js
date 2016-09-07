75a976259aadbff570e7a77f753cf3f6a6645cf8
Geoffrey Barnard <geoffreybans@gmail.com>
2016-09-07 17:19:04 +0300

    Displaying the marks entry page after user selects options

--- public/js/views/exams/entermarks.js
+++ public/js/views/exams/entermarks.js
@@ -6,12 +6,15 @@ define([
 	'collections/tmplt/streams',
 	'collections/tmplt/subjects',
 	'collections/tmplt/exams',
+	'collections/tmplt/marks',
 	'text!templates/exams/chooseexam.html',
 	'text!templates/exams/classes.html',
 	'text!templates/exams/streams.html',
 	'text!templates/exams/subjects.html',
-	'text!templates/exams/exams.html'
-	], function($, _, Backbone, Classes, Streams, Subjects, Exams, chooseExamTpl, classesTpl, streamsTpl, subjectsTpl, examsTpl){
+	'text!templates/exams/exams.html',
+	'text!templates/exams/marks-entries.html',
+	'text!templates/exams/marks-entry.html'
+	], function($, _, Backbone, Classes, Streams, Subjects, Exams, Marks, chooseExamTpl, classesTpl, streamsTpl, subjectsTpl, examsTpl, entriesTpl, entryTpl){
 
 	var Student = Backbone.View.extend({
 
@@ -20,15 +23,19 @@ define([
 		streamsTpl: _.template(streamsTpl),
 		subjectsTpl: _.template(subjectsTpl),
 		examsTpl: _.template(examsTpl),
+		entriesTpl: _.template(entriesTpl),
+		entryTpl: _.template(entryTpl),
 
 		tagName: 'div',
 
 		events: {
-			'change #class_id' : 'getStreams'
+			'change #class_id' : 'getStreams',
+			'submit form#entermarks' : 'enterMarks'
 		},
 
 		initialize: function(){
 			
+			this.$main = $(".container-fluid");
 			//fetch list of all classes for this class from the database
 			var self = this;
 			Classes.fetch({
@@ -142,7 +149,68 @@ define([
 				regExams: regExams
 			}));
 
+		},
+
+		enterMarks: function(evt){
+
+			//prevent default form submission
+			evt.preventDefault();
+
+			//get the selected values
+			var selectedOps = {
+				class: $("#class_id").val(),
+				stream: $("#stream_id").val(),
+				subject: $("#subject_id").val(),
+				exam: $("#exam_id").val(),
+				term: $("#term_id").val(),
+				year: $("#year").val()
+			};
+			var selected = {};
+			//populate with the actual models/objects
+			selected.class = Classes.where({id: selectedOps.class})[0].toJSON();
+			selected.stream = Streams.where({id: selectedOps.stream})[0].toJSON();
+			selected.subject = Subjects.where({id: selectedOps.subject})[0].toJSON();
+			selected.exam = Exams.where({id: selectedOps.exam})[0].toJSON();
+			selected.term = selectedOps.term;
+			selected.year = selectedOps.year;
+
+			//get the registered exams for this class
+			var exs = Exams.where({
+				class_id: $("#class_id").val()
+			});
+			var exms = [];
+			$.each(exs, function(key, ex){
+				exms.push(ex.toJSON());
+			});
+			selected.exams = exms;
+
+			//load the template into view
+			this.$main.html(this.entriesTpl({
+				selected: selected
+			}));
+
+			//fetch the list of students with marks, if they already have
+			var self = this;
+			Marks.fetch({
+				data: $.param({ 
+					token: tokenString,
+					class: selectedOps.class,
+					stream: selectedOps.stream,
+					subject: selectedOps.subject,
+					exam: selectedOps.exam,
+					term: selectedOps.term,
+					year: selectedOps.year					
+				}),
+				success: self.loadMarks()
+			});				
+
+		},
+
+		//display student marks after they are return, with option for entering new
+		loadMarks: function(){
+			
 		}
+
 	});
 
 	return Student;
