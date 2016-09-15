293a9ceb13afe48119749b348a2b12a758fbab9a
Geoffrey Barnard <geoffreybans@gmail.com>
2016-09-15 15:50:27 +0300

    Creating the models and collections for containing client/school information

---
+++ public/js/collections/users/clients.js
@@ -0,0 +1,25 @@
+define([
+	'underscore',
+	'backbone',
+	'models/user/client'
+	], function(_, Backbone, Client){
+	
+	Backbone.emulateHTTP = true;
+	Backbone.emulateJSON = true;
+	
+	var ClientsCollection = Backbone.Collection.extend({
+
+		model: Client,
+		
+		url: function () {
+			return baseURL + "admin/clients";
+		},
+
+		//todos sorted by their original insertion order
+		comparator: 'order'
+
+	});
+
+	return new ClientsCollection();
+
+});
